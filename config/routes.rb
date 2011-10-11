0d67186628fe104d206f224ea03a98df0fb00e23
Baskin Tapkan <baskint@tapkan.com>
2011-10-11 01:09:05 -0500

    added front page w/ sample content

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ BoardSports::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "board_sports#windsurf"
 
   # See how all your routes lay out with "rake routes"
 
