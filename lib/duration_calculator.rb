914d9bb7511a52b6503c1079899ab40c27b33a50
Baskin Tapkan <baskint@gmail.com>
2021-05-04 21:29:07 -0500

    duration computation complete

--- lib/duration_calculator.rb
+++ lib/duration_calculator.rb
@@ -1,5 +1,7 @@
 class DurationCalculator
   def self.compute(input)
-    6
+    h, m, s, ms = input.split(":").map(&:to_i)
+    h %= 24
+    ((h * 60) + m) + (s >= 30 ? 1 : 0)
   end
 end
--- spec/lib/duration_calculator_spec.rb
+++ spec/lib/duration_calculator_spec.rb
@@ -3,9 +3,21 @@ require "duration_calculator"
 
 describe DurationCalculator do
   describe ".compute" do
-    context "given an hours:minutes:seconds string" do
-      it "returns an integer with rounded to minutes" do
+    context "given an hours:minutes:seconds:ms string" do
+      it "returns an integer(1) with rounded to minutes" do
         expect(DurationCalculator.compute("00:05:45.3")).to eq(6)
+      end
+
+      it "returns an integer(2) with rounded to minutes" do
+        expect(DurationCalculator.compute("02:12:50")).to eq(133)
+      end
+
+      it "returns integer(3) with rounded to minutes" do
+        expect(DurationCalculator.compute("02:23:38")).to eq(144)
+      end
+
+      it "returns integer(4) with rounded to minutes" do
+        expect(DurationCalculator.compute("00:02:49.5")).to eq(3)
       end
     end
   end
